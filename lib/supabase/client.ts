f7073b50fbe472bd1c9cf4089dbe4638e982d2aa
zahirk100 <zahir55399@gmail.com>
2026-04-30 15:24:48 +0200

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -7,11 +7,13 @@ export function createClient() {
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
-      cookies: {
-        getAll() {
-          return cookieStore.getAll();
-        },
-        setAll(cookiesToSet) {
+      setAll(
+  cookiesToSet: {
+    name: string;
+    value: string;
+    options?: any;
+  }[]
+) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
               cookieStore.set(name, value, options)
